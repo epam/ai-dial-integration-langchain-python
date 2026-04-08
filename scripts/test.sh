2cf77ddb2f5f7ddf6b19a3a9685053fbabe9e584
Anton Dubovik <anton_dubovik@epam.com>
2026-04-08 13:57:49 +0100

    fix: reduce concurrency for python 3.13

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,13 @@ PY_FACTOR=${PYTHON:+py${PYTHON//./}}
 echo "PYTHON=$PYTHON"
 echo "PY_FACTOR=$PY_FACTOR"
 
-"$UV" tool run tox -p auto \
+if [[ "$PYTHON" == "3.13" ]]; then
+  PARALLEL=1
+else
+  PARALLEL=auto
+fi
+
+"$UV" tool run tox -p ${PARALLEL} \
   --parallel-no-spinner \
   -f test_openai $PY_FACTOR \
   -f test_monkey_patch $PY_FACTOR
