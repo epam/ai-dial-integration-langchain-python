17f0636da1d0d95c70f14e215ab967ebcd8d7fcc
Anton Dubovik <anton_dubovik@epam.com>
2026-04-08 15:44:00 +0100

    chore: reduce number of test-suites

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,17 +3,12 @@ set -x
 
 PY_FACTOR=${PYTHON:+py${PYTHON//./}}
 
-if [[ "$PYTHON" == "3.13" ]]; then
-  PARALLEL=1
-else
-  PARALLEL=auto
-fi
-
 echo "PYTHON=$PYTHON"
 echo "PY_FACTOR=$PY_FACTOR"
 echo "PARALLEL=$PARALLEL"
 
-"$UV" tool run tox --parallel ${PARALLEL} \
+"$UV" tool run tox \
+  --parallel auto \
   --parallel-no-spinner \
   --parallel-live \
   -f test_openai $PY_FACTOR \
