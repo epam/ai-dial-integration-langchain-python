3d385688489452dc560f077de294a070763b3a1b
Anton Dubovik <anton_dubovik@epam.com>
2026-04-08 14:13:07 +0100

    fix: small fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,14 +2,16 @@
 set -x
 
 PY_FACTOR=${PYTHON:+py${PYTHON//./}}
-echo "PYTHON=$PYTHON"
-echo "PY_FACTOR=$PY_FACTOR"
 
 if [[ "$PYTHON" == "3.13" ]]; then
   PARALLEL=1
 else
   PARALLEL=auto
 fi
+
+echo "PYTHON=$PYTHON"
+echo "PY_FACTOR=$PY_FACTOR"
+echo "PARALLEL=$PARALLEL"
 
 "$UV" tool run tox -p ${PARALLEL} \
   --parallel-no-spinner \
