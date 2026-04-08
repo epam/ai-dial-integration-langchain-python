1dedd3219d83e6ad9495ce6f6fdf21bf17f932b7
Anton Dubovik <anton_dubovik@epam.com>
2026-04-08 12:33:47 +0100

    chore: limit parallelism

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ PY_FACTOR=${PYTHON:+py${PYTHON//./}}
 echo "PYTHON=$PYTHON"
 echo "PY_FACTOR=$PY_FACTOR"
 
-"$UV" tool run tox -p 4 \
+"$UV" tool run tox -p auto \
   --parallel-no-spinner \
   -f test_openai $PY_FACTOR \
   -f test_monkey_patch $PY_FACTOR
